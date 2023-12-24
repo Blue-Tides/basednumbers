2cbb59ee6f48a6290ca8f32f135f289cd6b2eac5
Blue-Tides <71620668+Blue-Tides@users.noreply.github.com>
2023-12-23 23:51:26 -0800

    modified:   Number.java 	new file:   TestNumber.java 	new file:   lib/hamcrest-core-1.3.jar         new file:   lib/junit-4.13.2.jar

--- Number.java
+++ Number.java
@@ -1,16 +1,78 @@
 public class Number {
   private int b; //base
-  private int v; //value
+  private int v; //value in 
+  private int log(int x) {
+    return (int)(Math.log10(x)/Math.log10(b));
+  }
+  /**
+   * New number in base 10
+   * @param v
+   */
   public Number(int v) {
     this.v=v;
     this.b=10;
   }
+  /**
+   * @param v value in base 10
+   * @param b base
+   */
   public Number(int v, int b) {
     this.v=v;
     this.b=b;
   }
+  /**
+   * Makes a deep copy of n
+   */
   public Number(Number n) {
     b=n.b;
     v=n.v;
   }
+  /**
+   * @return the value in string form in its current base
+   */
+  public String getValue() {
+    if(v==0) return "0";
+    String a="";
+    int v=this.v;
+    for(int i=log(v);i>=0;i--) {
+      int t=(int)Math.pow(b,i);
+      int x=v/t;
+      a+=(x<10)?""+x:(char)(x+55);
+      v=v%t;
+    }
+    return a; //for compiler
+  }
+  /**
+   * @return the value in base 10
+   */
+  public int getValue10() {
+    return v;
+  }
+  public int getBase() {
+    return b;
+  }
+  /**
+   * @param v in base 10
+   */
+  public void setValue(int v) {
+    this.v=v;
+  }
+  public void setValue(Number n) {
+    this.v=n.v;
+  }
+  /**
+   * @param s the number in its current base
+   */
+  public void setValue(String s) {
+    v=0;
+    int l=s.length();
+    for(int i=0;i<l;i++) {
+      char x=s.charAt(i);
+      v+=(int)(x-(Character.isDigit(x)?'0':0))*Math.pow(b,l-i-1);
+    }
+  }
+  public String toString() {
+    return "Value_10 of: "+v+"\nBase: "+b;
+  }
+
 }
---
+++ TestNumber.java
@@ -0,0 +1,45 @@
+public class TestNumber extends junit.framework.TestCase {
+    public void testConstructors() {
+        Number x=new Number(4);
+        //actually im too lazy lets assume they work :P
+    }
+    public void testGetValueBase2() {
+        Number base2=new Number(1,2);
+        assertEquals("1",base2.getValue());
+        base2.setValue(2);
+        assertEquals("10",base2.getValue());
+        base2.setValue(7);
+        assertEquals("111",base2.getValue());
+        base2.setValue(13);
+        assertEquals("1101",base2.getValue());
+        base2.setValue(391);
+        assertEquals("110000111", base2.getValue());
+        base2.setValue(0);
+        assertEquals("0",base2.getValue());
+    }
+    public void testGetValueBase16() 
+    {
+        Number base16=new Number(13,16);
+        assertEquals("D",base16.getValue());
+        base16.setValue(130);
+        assertEquals("82",base16.getValue());
+        base16.setValue(1567);
+        assertEquals("61F", base16.getValue());
+        base16.setValue(1);
+        assertEquals("1",base16.getValue());
+    }
+    public void testSetValueBase2() {
+        Number base2=new Number(1,2);
+        base2.setValue("1010");
+        assertEquals(10,base2.getValue10());
+        base2.setValue("1111");
+        assertEquals(15,base2.getValue10());
+        base2.setValue("110000111");
+        assertEquals(391,base2.getValue10());
+        base2.setValue("000000110000111");
+        assertEquals(391,base2.getValue10());
+    } 
+    public void testSetBase16() {
+
+    }
+}
